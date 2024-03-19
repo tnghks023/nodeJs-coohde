76f38467aecbe187c1b28c0e1c6d53c6200420f8
noob <setking929@gmail.com>
2024-03-19 10:44:03 +0900

    reduce Code

--- main.js
+++ main.js
@@ -2,6 +2,32 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) {
+
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `
+}
+
+function templateList(filelist) {
+  var list = '<ul>';
+  filelist.forEach(file => {
+    list += `<li><a href="/?id=${file}">${file}</a></li>`;
+  })
+  list += '</ul>';
+  return list;
+}
 
 var app = http.createServer((request, response) => {
   var _url = request.url;
@@ -20,28 +46,11 @@ var app = http.createServer((request, response) => {
 
       fs.readdir('data', (err, filelist) => {
 
-        var list = '<ul>';
-        filelist.forEach(file => {
-          list += `<li><a href="/?id=${file}">${file}</a></li>`;
-        })
-        list += '</ul>';
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>
-          ${description}
-          </p>
-        </body>
-        </html>
-        `;
+
+        var list = templateList(filelist)
+
+        var template = templateHTML(title, list,
+          `<h2>${title}</h2>${description}`);
 
         response.writeHead(200);
         response.end(template);
