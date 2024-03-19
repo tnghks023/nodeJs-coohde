6d9c5f02d6a207d255f47d84e90fe02ed7bcfff8
noob <setking929@gmail.com>
2024-03-19 10:50:03 +0900

    sync async example

---
+++ syntax/sync.js
@@ -0,0 +1,15 @@
+const fs =require('fs');
+
+//readFileSync
+// console.log('A');
+// var result = fs.readFileSync('syntax/sample.txt', 'utf-8');
+// console.log(result);
+// console.log('C');
+
+
+//readFile
+console.log('A');
+fs.readFile('syntax/sample.txt', 'utf-8', (err, data) => {
+    console.log(data)
+});
+console.log('C');
