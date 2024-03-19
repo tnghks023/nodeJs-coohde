66125fd71618af231042b5e17dd9ec631087e8f7
noob <setking929@gmail.com>
2024-03-19 23:07:34 +0900

    module example

--- main.js
+++ main.js
@@ -2,34 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
+var template = require('./lib/template.js')
 
-var template = {
-  HTML: (title, list, body, control) => {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    `
-  }, 
-  LIST : (filelist) => {
-    var list = '<ul>';
-    filelist.forEach(file => {
-      list += `<li><a href="/?id=${file}">${file}</a></li>`;
-    })
-    list += '</ul>';
-    return list;
-  }
-}
 
 var app = http.createServer((request, response) => {
   var _url = request.url;
