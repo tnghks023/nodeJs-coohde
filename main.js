69847a6182135cb95ce347bb9ac3c9a642e064c5
noob <setking929@gmail.com>
2024-03-19 09:58:36 +0900

    not found

--- main.js
+++ main.js
@@ -2,45 +2,47 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+
 var app = http.createServer((request, response) => {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname
   var title = queryData.id
-  console.log(queryData.id)
-  if (_url == '/') {
-    title = 'Welcome';
-  }
-  if (_url == '/favicon.ico') {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  fs.readFile(`data/${title}`, 'utf-8', (err, description) => {
-    
 
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - HTML</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">${title}</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="?id=CSS">CSS</a></li>
-        <li><a href="?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>
-      ${description}
-      </p>
-    </body>
-    </html>
-    `;
+  if (pathname == '/') {
+    fs.readFile(`data/${title}`, 'utf-8', (err, description) => {
 
-    response.end(template);
-  })
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - HTML</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">${title}</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="?id=CSS">CSS</a></li>
+          <li><a href="?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>
+        ${description}
+        </p>
+      </body>
+      </html>
+      `;
+
+      response.writeHead(200);
+      response.end(template);
+    })
+
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
+
 });
 
 app.listen(3000);
